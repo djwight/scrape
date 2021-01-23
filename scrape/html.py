3f4e99587b9f7d35e6c5215d652909d32ceddbf4
djwight <d.j.wight@gmail.com>
2021-01-23 18:54:19 +0100

    added documentation with pydoc

--- scrape/html.py
+++ scrape/html.py
@@ -11,11 +11,9 @@ import pandas as pd
 
 
 class scraper():
-    """
-    A class to create a scraper for html pages and extraction of relevant
+    """Class to create a scraper for html pages and extraction of relevant
     data. Relies on Chrome for webscraping please insure that the chromedriver
-     is installed locally.
-    """
+    is installed locally."""
 
     def __init__(self, chrome_drive_path):
         self.chrome_drive_path = chrome_drive_path
@@ -25,6 +23,15 @@ class scraper():
         self.driver = None
 
     def create_session(self, base_url, save_dir):
+        """Initializes a session with the base url, dir to save data and the
+        selenium driver.
+
+        Parameters
+        ----------
+        base_url : string
+            Base url to be scraped.
+        save_dir : string
+            Dir where results from scraping should be saved."""
         try:
             self.base_dir = Path(save_dir)
         except OSError as error:
@@ -36,6 +43,15 @@ class scraper():
         self._initialize_selenium()
 
     def add_scrape_list(self, json_file, sub_domain_element):
+        """Reads a json containing the list of subpages to scrape and saves it
+        into the instance.
+
+        Parameters
+        ----------
+        json_file : string
+            Path and file name for the json file with the list of pages.
+        sub_domain_element : string
+            Name of the element in the json that cotains the list."""
         try:
             with open(json_file) as jf:
                 pages_data = json.load(jf)
@@ -44,25 +60,20 @@ class scraper():
             print(error)
 
     def _ping(self, url):
-        """
-        Checks that the provide base url is live.
+        """Checks that the provide base url is live.
 
         Parameters
         ----------
         url : string
-            Base url to be scraped.
-        """
+            Base url to be scraped."""
         param = '-n' if platform.system().lower() == 'windows' else '-c'
         command = ['ping', param, '1', url]
         output = subprocess.call(command, stdout=subprocess.DEVNULL) == 0
         return output
 
     def _initialize_selenium(self):
-        """
-        Initializes the selenium driver for scraping through Chrome.
-        """
+        """Initializes the selenium driver for scraping through Chrome."""
         options = Options()
         options.headless = True
         options.add_argument("--window-size=1920,1200")
-        self.driver = webdriver.Chrome(options=options, /
-                                    executable_path=self.chrome_drive_path)
+        self.driver = webdriver.Chrome(options=options, executable_path=self.chrome_drive_path)
